b6c72a6c7a6d6a71317364c88d015d76d1ad9dc9
solittleceo <gapordie2@gmail.com>
2023-04-20 17:56:53 +1000

    keys aboba

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { FC } from "react";
 import navItems from "./navigationData";
+import { useRouter } from "next/router";
 
 
 const Navigation: FC = () => {
+    const router = useRouter()
+    console.log(router)
+
     return (
         <nav className='pt-10 flex justify-center'>
             <ul className='text-3xl flex space-x-10'>
@@ -12,7 +16,8 @@ const Navigation: FC = () => {
                         return (
                             <li key={navItem.text}>
                                 <Link
-                                    className="transition ease-in-out duration-500 hover:text-cyan-700"
+                                    className={`transition ease-in-out duration-500 hover:text-cyan-700 
+                                    ${router.pathname===navItem.url ? 'text-emerald-500' : ''}`}
                                     href={`${navItem.url}`}
                                 >
                                     {navItem.text}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import { ChangeEvent, useEffect, useState } from "react"
+import { debug } from "util"
+
 export default function Home() {
+  const [data, setData] = useState<string>('init data')
+
+  useEffect(() => {
+    // alert("Cat")
+    const testDiv: HTMLDivElement | null = document.querySelector('#test')
+    const date: string = new Date(Date.now()).toString()
+    testDiv!.innerText = date
+  }, [data])
+
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event)
+    setData(event.target.value)
+  }
+
   return (
     <>
-      <div className="h-screen">
+      <div className="h-screen flex flex-col items-center justify-center">
+
+        <div className="text-4xl" id="test"></div>
+
+        <input className='m-24 text-3xl border-4' type="text" value={data} onChange={(event) => handleOnChange(event)}/>
         <div className="h-3/4 flex justify-center items-center tx-xl">Главная</div>
       </div>
     </>
